1399a7283d42414fa81d86031dfe4e337191cdc9
HenriettaObioha <Henriettaobioha1@gmail.com>
2023-02-17 06:04:39 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,18 @@
-#include "holberton.h"
+#include <stdio.h>
+
 /**
- * main - Entry point
- *
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *sh = "Holberton";
+	char ch;
 
-	while (*sh)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		_putchar(*sh);
-		sh++;
+		putchar(ch);
 	}
-	_putchar('\n');
-
+	putchar('\n');
 	return (0);
 }
